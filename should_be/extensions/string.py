a06e0e3ff17d37f1d4a6c05e620240f77a86025b
Solly Ross <sross@redhat.com>
2014-09-08 17:07:49 -0400

    Made string not inherit sequence's should_be

--- should_be/extensions/string.py
+++ should_be/extensions/string.py
@@ -15,3 +15,9 @@ class StringMixin(BaseMixin):
     def shouldnt_match(self, target):
         msg = '{txt} should not have matched {re}, but did anyway'
         self.should_follow(re.match(target, self) is None, msg, re=target)
+
+    # explicitly override this so it has precedence over sequence
+    # and quotes around values
+    def should_be(self, target):
+        msg = '{txt} should have been "{val}", but was "{self}"'
+        self.should_follow(self == target, msg, val=target)
