dd24185e2f81653f8a4c5082788432cdddebe34a
Solly Ross <sross@redhat.com>
2014-10-31 18:33:30 -0400

    Made Set Extension Comparison More Generic
    Previously, the set extension compared the differences with
    `set()` to check for the empty set.  This did not work for
    subclasses of `collections.Set`, so now it uses `len()` to check
    for empty sets.

--- should_be/extensions/set.py
+++ should_be/extensions/set.py
@@ -17,17 +17,17 @@ class SetMixin(BaseMixin):
             we_had = self - target
             they_had = target - self
 
-            if (we_had != set() and they_had != set()):
-                self.should_follow(they_had == they_had == set(), msg_diff,
+            if (len(we_had) != 0 and len(they_had) != 0):
+                self.should_follow(len(we_had) == len(they_had) == 0, msg_diff,
                                    val=target,
                                    i1=we_had,
                                    i2=they_had)
 
-            self.should_follow(we_had == set(), msg_bigger,
+            self.should_follow(len(we_had) == 0, msg_bigger,
                                val=target,
                                items=we_had)
 
-            self.should_follow(they_had == set(), msg_smaller,
+            self.should_follow(len(they_had) == 0, msg_smaller,
                                val=target,
                                items=they_had)
         except TypeError:
