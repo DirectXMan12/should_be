f36774d6822c5bf292ba29fafc1c48139e69040a
Solly Ross <sross@redhat.com>
2015-01-05 16:34:53 -0500

    Prepare for publishing
    Converted the license to ISC, and fixed up setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python2.7
-import setuptools
 from setuptools import setup
-from setuptools import Extension
 
 setup(
-    name='ShouldBe',
+    name='shouldbe',
     version='0.1.0',
     author='Solly Ross',
     author_email='sross@redhat.com',
@@ -12,8 +10,23 @@ setup(
     description='Python Assertion Helpers inspired by Shouldly',
     long_description=open('README.txt').read(),
     license='LICENSE.txt',
+    url="https://github.com/directxman12/should_be",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     install_requires=[
         'forbiddenfruit'
-    ]
+    ],
+    tests_require=[
+        'nose'
+    ],
+    keywords=['testing', 'assertions'],
 )
-
